143960c1b80c78df7350bcc4d0ca5f1eef35cfc0
viper12590 <viper1259@gmail.com>
2021-08-18 09:11:02 +0100

    show multiple files as preview

--- frontend/src/components/ItemList.vue
+++ frontend/src/components/ItemList.vue
@@ -5,7 +5,21 @@
     <p class="mb-1">{{Intl.DateTimeFormat("en-GB",dateTimeFormat).format(new Date(item.uploadDate))}}</p>
     <p class="mb-1">Favourite: {{item.favourite}}</p>
     <p>{{counterName}}: {{item.score}}</p>
-    <p><a v-bind:href="'/files/'+item.files[0]"><img class="img-fluid" v-bind:src="'/files/'+item.files[0]" v-bind:alt="item.name"></a></p>
+    <div v-if="item.files.length < 3">
+      <p v-for="(file, index) in item.files" v-bind:key="index">
+        <a v-bind:href="'/files/'+ file"><img class="img-fluid" v-bind:src="'/files/'+ file" v-bind:alt="item.name"></a>
+      </p>
+    </div>
+    <div v-else>
+      <p>
+        <a v-bind:href="'/files/'+item.files[0]"><img class="img-fluid" v-bind:src="'/files/'+item.files[0]" v-bind:alt="item.name"></a>
+      </p>
+      <div class="row">
+        <div v-for="(file, index) in item.files.slice(1)" v-bind:key="index" class="col">
+          <a v-bind:href="'/files/'+ file"><img class="img-fluid" v-bind:src="'/files/'+ file" v-bind:alt="item.name"></a>
+        </div>
+      </div>
+    </div>
     <p class="mb-5">{{item.memo}}</p>
   </article>
 
