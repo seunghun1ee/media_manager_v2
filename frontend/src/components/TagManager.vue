463a9c947a190f565cac7bf582d3b9e9e28de336
viper12590 <viper1259@gmail.com>
2021-08-22 06:39:58 +0100

    reload after saving tag

--- frontend/src/components/TagManager.vue
+++ frontend/src/components/TagManager.vue
@@ -1,5 +1,7 @@
 <template>
   <h3>Create new tag</h3>
+  <Loading v-if="isLoading"></Loading>
+  <p v-if="isSubmitted">New tag was created</p>
   <form id="newTagForm" v-on:submit="onSubmit">
     <div class="mb-1">
       <label for="newTagNameInput" class="form-label">New tag value</label>
@@ -28,21 +30,25 @@
 
 <script>
 import {postCreateTag} from "@/repository";
+import Loading from "@/components/Loading";
 
 export default {
   name: "TagManager",
+  components: {Loading},
   data() {
     return {
       newTagValue: null,
       newTagType: "person",
       newTagLabel: null,
-      newTagDescription: null
+      newTagDescription: null,
+      isLoading: false,
+      isSubmitted: false
     }
   },
   methods: {
     onSubmit(event) {
       event.preventDefault();
-
+      this.isLoading = true;
       let tagData = {
         value: this.newTagValue,
         type: this.newTagType,
@@ -52,6 +58,9 @@ export default {
       postCreateTag(tagData)
           .then(res => {
             console.log(res);
+            this.isLoading = false;
+            this.isSubmitted = true;
+            location.reload();
           })
           .catch(err => {
             console.error(err.response.headers);
